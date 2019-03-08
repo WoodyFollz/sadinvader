9587dadc60cc9470746130d43f8af64909664124
Danny Hpy (Android) <danny@android.nul>
2019-03-08 18:21:49 +0100

    Ignore aliases if there are none

--- utils/commandhandler.js
+++ utils/commandhandler.js
@@ -5,8 +5,8 @@ exports.add = (obj) => commands[commands.length] = obj
 exports.get = function get(name) {
   //check: exact name
   var command = commands.filter(x => {
-    return x.name === name || (x.aliases && x.aliases.includes(name))[0]
-  })
+    return x.name === name || (x.aliases && x.aliases.includes(name))
+  })[0]
   if (command) return [command]
   
   //check: starting name with
