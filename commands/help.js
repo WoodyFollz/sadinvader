ef585083f5087c3c5893c48a462edb2418d08a9c
Danny Hpy <danharp.contact@gmail.com>
2019-03-17 08:08:43 +0100

    [commands.help] Initial version.

---
+++ commands/help.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+
+/* ==========================
+This is a first version. ====
+Contributions are welcomed. =
+========================== */
+async function helpCommand(message) {
+  const cmds = this.bot.commands.stored
+
+  var desc  = cmds.map(cmd => `**${cmd.name}**: ${cmd.desc || 'No description available.'}`)
+  var embed = new Discord.RichEmbed()
+              .setTitle('Command List')
+              .setColor(this.guild.me.displayHexColor)
+              .setDescription(desc.join('\n'))
+              .setFooter(`${cmds.length} commands available!`)
+}
+
+module.exports = () => [
+  {
+    name: 'help',
+    desc: 'Get the list of available commands.',
+    callback: helpCommand
+  }
+]
